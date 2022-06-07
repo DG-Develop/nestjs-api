679b3654d9004c68b6d4f887c007847a21d8f6ec
DG-Develop <dgdband@gmail.com>
2022-06-07 18:52:46 -0500

    [ADD] Agregando passport

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -4,10 +4,14 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from './../../users/services/users.service'
 import * as bcrypt from 'bcrypt'
 
+import { User } from 'src/users/entities/user.entity';
+import { PayloadToken } from '../models/token.model';
+import { JwtService } from '@nestjs/jwt'
+
 @Injectable()
 export class AuthService {
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
   async validateUser(email: string, password: string) {
     const user = await this.usersService.findByEmail(email);
@@ -22,6 +26,15 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  generateJWT(user: User){
+    const payload: PayloadToken = { role: user.role, sub: user.id }
+
+    return {
+      access_token : this.jwtService.sign(payload),
+      user
+    }
   }
 
 }
